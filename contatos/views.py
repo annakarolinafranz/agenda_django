f895cd58e4e6231cd970d7d8883919ec633ec651
annakarolinafranz <nerdstecnologia0@gmail.com>
2021-08-26 01:07:49 -0300

    developing app in Python and Django, list schedule phone

---
+++ contatos/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Contato
+
+def index(request):
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+def ver_contato(request, contato_id):
+    # try:
+    # contato = Contato.objects.get(id=contato_id)  # get busca um objeto e o argumento qual campo que eu to buscando ex o 'id'
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+        })
+    # except Contato.DoesNotExist as e:
+    #     raise Http404()
